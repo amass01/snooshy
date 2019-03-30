1d70dfe139e084a626860cbb1c212104a27242ef
Amir Massarwa <amassarwi@gmail.com>
2019-03-30 22:55:10 +0100

    App setup & basic nav-bar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { MatToolbarModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+  ],
+})
+export class SharedModule { }
