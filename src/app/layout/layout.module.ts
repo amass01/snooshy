f3d728098cb0947465ec68fd946ca114b6b7716c
Amir Massarwa <amassarwi@gmail.com>
2019-03-31 02:31:09 +0100

    Added variables scss file and home componet

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     ToolBarComponent,
+    HomeComponent,
   ],
   imports: [
     SharedModule,
