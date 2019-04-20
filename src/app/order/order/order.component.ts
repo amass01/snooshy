4d389f80224629731203afd235ba30bc867fc204
Amir Massarwa <amassarwi@gmail.com>
2019-04-20 16:30:05 +0200

    Display products in order page dynamically

--- src/app/order/order/order.component.ts
+++ src/app/order/order/order.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Products {
+  starters: Product[],
+}
+
 interface Product {
   name: string;
   price: number;
-  description: string;
+  description?: string;
+  imgPath: string;
 }
 
 @Component({
@@ -13,16 +18,61 @@ interface Product {
 })
 export class OrderComponent implements OnInit {
 
-  public products: Product[]; 
+  public products: Products; 
 
   constructor() {
-    this.products = [
-      {
-        name: 'חמוצים יפנים',
-        price: 10,
-        description: '',
-      }
-    ]
+    this.products = {
+      starters: [
+        {
+          name: 'חמוצים יפנים',
+          price: 10,
+          imgPath: 'starters/tona.jpg',
+        },
+        {
+          name: 'אדממה',
+          price: 15,
+          imgPath: 'starters/tona.jpg',
+        },
+        {
+          name: 'נאגטס עוף',
+          price: 25,
+          imgPath: 'starters/tona.jpg',
+        },
+
+        {
+          name: 'הום פרייז',
+          price: 15,
+          imgPath: 'starters/tona.jpg',
+        },
+
+        {
+          name: 'אגרול',
+          price: 15,
+          imgPath: 'starters/tona.jpg',
+        },
+
+        {
+          name: 'כנפיים',
+          price: 25,
+          imgPath: 'starters/tona.jpg',
+        },
+        {
+          name: "סביצ'ה סלמון",
+          price: 30,
+          imgPath: 'starters/tona.jpg',
+        },
+        {
+          name: "ברוסקטה טונה",
+          price: 30,
+          description: '',
+          imgPath: 'starters/tona.jpg',
+        },
+      ],
+    }
+  }
+
+  getImageFullUrl(imgPath: string) {
+    return `url(../../../assets/img/products/hd/${imgPath})` 
   }
 
   ngOnInit() {
